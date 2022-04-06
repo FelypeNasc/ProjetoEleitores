c7d1b7c4c628961ad02833abd47e6a5d669a17b3
Dunas-Lopiin <plinio.figueiredo.s@gmail.com>
2022-04-06 14:46:05 -0300

    verificação de envio

--- backend/routes/read.js
+++ backend/routes/read.js
@@ -2,11 +2,7 @@ const router = require('express').Router();
 const validations = require('../middlewares/validations.js');
 const { Client } = require('pg');
 
-router.get('/:tipo/:id/:id2', validations.validateRead, (req, res) => {
-	console.log(req.params.tipo);
-	console.log(req.params.id);
-	console.log(req.params.id2);
-	
+router.get('/pesquisa', validations.validateRead, (req, res) => {		
 	const client = new Client({
 		user: 'postgres',
 		password: 'toRyca123',
@@ -17,53 +13,56 @@ router.get('/:tipo/:id/:id2', validations.validateRead, (req, res) => {
 
 	let table;
 	let query;
-	client.connect()
-		.then(() => console.log("conectado ao banco"))
-		.then(() => {
-			let tipo = parseInt(req.params.tipo);
-			console.log(tipo);
-			let id = parseInt(req.params.id);
-			let id2 = parseInt(req.params.id2);
+	let tipo = req.query.tipo;
+	let id = parseInt(req.query.id);
+	let id2 = parseInt(req.query.id2);
+	let connect = true;
+	
+	switch(tipo){
+		case "1":
+			table = "renda_id";
+			break;
+		case "2":
+			table = "nivel_escolar_id";
+			break;
+		default:
+			connect = false;
+			res.send("pesquisa não encontrada, cancelando pesquisa");
+			return false;
+			break;
+	}
+	if(isNaN(id)){
+		id = 1;
+	}
+	if(isNaN(id2)){
+		id2 = 20;
+	}
 
-			
-			switch(tipo){
-				case 1:
-					table = "renda_id";
-					break;
-				case 2:
-					table = "nivel_escolar_id";
-					break;
-				default:
-					table = "nda";
-					break;
-			}
-		
-			if(id < 1){
-				res.send('pesquisa invalida, o valor é menor que 1!');
-			}
-			else if(id2 < id){
-				res.send('pesquisa invalida, o segundo valor é menor que o primeiro!');
-			}
-		
-			if(table === "nda"){
-				res.send("pesquisa não encontrada");
-			}
-			console.log(table);
-		
-			if(id2 && id < id2){
-				query = `SELECT * FROM public.eleitores WHERE ${table} BETWEEN ${id} AND ${id2}`;
-				console.log(query);
-			}
-			else{
-				query = `SELECT id, nome FROM public.eleitores WHERE ${table} = ${id}`;
-				console.log(query);
-			}
-		})
+	if(id < 1){
+		connect = false;
+		res.send('pesquisa invalida, o valor é menor que 1!');
+	}
+	else if(id2 < id){
+		connect = false;
+		res.send('pesquisa invalida, o segundo valor é menor que o primeiro!');
+	}
+
+	if(id2 && id < id2){
+		query = `SELECT * FROM public.eleitores WHERE ${table} BETWEEN ${id} AND ${id2}`;
+	}
+	else{
+		query = `SELECT id, nome FROM public.eleitores WHERE ${table} = ${id}`;
+	}
+
+	if(connect){
+		client.connect()
+		.then(() => console.log("conectado ao banco"))
 		.then(() => client.query(query))
 		.then(results => results.rows)
 		.then(feedback => res.send(feedback))
 		.catch(e => console.log(e))
 		.finally(() => client.end(), console.log("cliente fechado"))
+	}
 });
 
 module.exports = router;
