d7849a94c89fbdeb51a06a5f683b74cbe2879e6e
Luan Raabe <luan.raabe202@gmail.com>
2022-04-06 00:50:21 -0300

    FIX delete route

--- backend/config/postgres.js
+++ backend/config/postgres.js
@@ -1,12 +1,11 @@
-const { Client } = require('pg');
 require('dotenv').config({ path: __dirname + '/.env' });
 
-const client = new Client({
+const conf = {
 	user: 'postgres',
 	password: process.env.POSTGRES_PASSWORD,
 	host: process.env.POSTGRES_HOST,
 	port: process.env.POSTGRES_PORT,
 	database: 'eleitores',
-});
+};
 
-module.exports = client;
+module.exports = conf;
--- backend/routes/delete.js
+++ backend/routes/delete.js
@@ -1,27 +1,27 @@
 const router = require('express').Router();
 const validations = require('../middlewares/validations.js');
-const client = require('../config/postgres.js');
+const { Client } = require('pg');
+const conf = require('../config/postgres.js');
 
 router.delete('/', validations.validateDelete, async (req, res) => {
-	res.send(await deleteUser(req.body.userId));
-});
-
-let query = 'UPDATE public.eleitores SET deletada = true WHERE id=$1';
-
-async function deleteUser(value) {
-	try {
-		await client.connect();
-		console.log('conectado ao banco');
-		await client.query(query, [value]);
-		await client.end();
-		return true;
-	} catch (e) {
-		await client.query('ROLLBACK');
-		console.log('erro:', e);
-	} finally {
-		await client.end();
-		console.log('Fim da conexão');
+	async function deleteUser(id) {
+		const client = new Client(conf);
+		try {
+			let query = 'UPDATE public.eleitores SET deletada = true WHERE id=$1';
+			await client.connect();
+			console.log('conectado ao banco');
+			await client.query(query, [id]);
+			await client.end();
+			return result.rows;
+		} catch (e) {
+			await client.query('ROLLBACK');
+			console.log('erro:', e);
+		} finally {
+			await client.end();
+			console.log('Fim da conexão');
+		}
 	}
-}
+	res.send(await deleteUser(req.query.userId));
+});
 
 module.exports = router;
