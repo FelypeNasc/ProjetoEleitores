acf29d821f3a7e77ed46fb120509220d76eb44bd
Luan Raabe <luan.raabe202@gmail.com>
2022-04-05 21:43:14 -0300

    Delete route

--- backend/routes/update.js
+++ backend/routes/update.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const validations = require('../middlewares/validations.js');
 
-router.update('/', validations.validateUpdate, (req, res) => {
+router.put('/', validations.validateUpdate, (req, res) => {
 	res.send({ msg: 'hellofriend! you just UPDATED a record.' });
 });
 
--- backend/server.js
+++ backend/server.js
@@ -16,4 +16,19 @@ app.use('/read', readRoute);
 app.use('/update', updateRoute);
 app.use('/delete', deleteRoute);
 
+const { Client } = require('pg');
+const client = new Client({
+    user: "postgres",
+    password: "toRyca123",
+    host: "lab.dudeful.com",
+    port: "5432",
+    database: "eleitores"
+});
+
+client.connect()
+.then(() => console.log("Working"))
+.catch((e) => console.log("error:", e))
+.finally(() => client.end());
+// Route Config
+
 app.listen(port, () => console.log('Server started on port ' + port));
