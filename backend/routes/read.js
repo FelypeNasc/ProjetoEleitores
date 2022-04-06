0c1cd9a88b42e938a287851a6df84024da5d1c2c
Dunas-Lopiin <plinio.figueiredo.s@gmail.com>
2022-04-05 21:40:57 -0300

    conexão com o banco de dados

--- backend/routes/read.js
+++ backend/routes/read.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 const validations = require('../middlewares/validations.js');
+const { Client } = require('pg');
+
+const client = new Client({
+	user: 'postgres',
+	password: 'lopiin20',
+	host: 'localhost',
+	port: 5432,
+	database: 'lovelace'
+})
 
 router.get('/', validations.validateRead, (req, res) => {
+	console.log(req);
+	let query = "SELECT nome FROM public.eleitores_nivel_escolar";
+	client.connect()
+		.then(() => console.log("conectado ao banco"))
+		.then(() => client.query(query))
+		.then( results => console.table(results.rows))
+		.catch(e => console.log(e))
+		.finally(() => client.end())
 	res.send({ msg: 'hellofriend! you just READ a record.' });
 });
 
--- backend/routes/update.js
+++ backend/routes/update.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 const validations = require('../middlewares/validations.js');
+const { Client } = require('pg');
 
-router.update('/', validations.validateUpdate, (req, res) => {
+router.put('/', validations.validateUpdate, (req, res) => {
+	const Client = new Client({
+		user: 'postgres',
+		password: 'lopiin20',
+		host: 'localhost',
+		port: 5432,
+		database: 'lovelace'
+	})
 	res.send({ msg: 'hellofriend! you just UPDATED a record.' });
 });
 
